37f733de392874b479d45f8f22b961b6327764ba
shaunsephton <shaun@praekeltconsulting.com>
2011-01-05 16:51:33 +0200

    added inline

--- playlist/admin.py
+++ playlist/admin.py
@@ -1,18 +1,21 @@
-
-## django imports
 from django.contrib import admin
 
-## our own app imports
-from playlist.models import AdactusPlaylist, DSTVPlaylist, ManualPlaylist, ContentPlaylist, PlaylistEntry
+from playlist.models import Playlist, ScheduledPlaylist, PlaylistEntry
 from panya.admin import ModelBaseAdmin
 
-class AdminContentPlaylist(admin.ModelAdmin):
-    list_display = ('__str__','content','playlist','commence','expire')
+class PlaylistEntryInline(admin.TabularInline):
+    model = PlaylistEntry
+
+class PlaylistAdmin(ModelBaseAdmin):
+    inlines = ModelBaseAdmin.inlines + [
+        PlaylistEntryInline,
+    ]
+
+class ScheduledPlaylistAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__','content','playlist','start','end')
     list_filter = ('content',)
     search_fields = ['playlist__title']
 
-admin.site.register(AdactusPlaylist,ModelBaseAdmin)
-admin.site.register(DSTVPlaylist,ModelBaseAdmin)
-admin.site.register(ManualPlaylist,ModelBaseAdmin)
-admin.site.register(PlaylistEntry,ModelBaseAdmin)
-admin.site.register(ContentPlaylist,AdminContentPlaylist)
+admin.site.register(Playlist, PlaylistAdmin)
+admin.site.register(PlaylistEntry)
+admin.site.register(ScheduledPlaylist, ScheduledPlaylistAdmin)
